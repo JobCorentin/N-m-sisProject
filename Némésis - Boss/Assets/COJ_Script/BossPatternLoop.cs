c2818b19e81d9e05cb08a1c74b9aa14f14623eed
Job Corentin <job.corentin@outlook.fr>
2019-10-27 18:29:12 +0100

    Attempt to make the boss pattern work (FAIL)

--- Némésis - Boss/Assets/COJ_Script/BossPatternLoop.cs
+++ Némésis - Boss/Assets/COJ_Script/BossPatternLoop.cs
@@ -10,9 +10,14 @@ public class BossPatternLoop : MonoBehaviour
     protected int patternRef;
     protected int patternSaved;
     protected bool canTakeDamage = true;
+    protected float seconds;
     protected float weaponDamage = 5;
     public Slider healthBar;
     List<int> patternList = new List<int>();
+
+    public GameObject leftArm01Prefab;
+    public Transform leftArmPositionPoint;
+    public Transform playerPosition;
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +29,20 @@ public class BossPatternLoop : MonoBehaviour
         }
         patternSaved = -1;
         BossPatternSelection();
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         healthBar.value = bossHealth;
+        seconds++;
+
+        if( patternRef == 0)
+        {
+            Debug.Log(seconds);
+            leftArm01Pattern();
+        }
     }
 
     void BossPatternSelection()
@@ -54,12 +67,14 @@ public class BossPatternLoop : MonoBehaviour
         if (patternRef == 0)
         {
             Debug.Log("Pattern ref = 0");
+            seconds = 0;
+
         }
         else
         {
             Debug.Log("pattern is 1 or 2");
         }
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(10f);
         RefreshPattern();
     }
 
@@ -88,8 +103,33 @@ public class BossPatternLoop : MonoBehaviour
         bossHealth -= weaponDamage;
         canTakeDamage = false;
         Debug.Log("bosshealth =" + bossHealth);
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(5f);
         canTakeDamage = true;
+
+    }
+
+
+    void leftArm01Pattern()
+    {
+        bool canDo = true;
+        if(seconds <= 75f)
+        {
+            leftArm01Prefab.transform.position = playerPosition.transform.position + new Vector3(0, 2, 0);
+            Debug.Log("wait for it");
+            
+        }
+        else if (seconds <= 125f && canDo == true)
+        {
+            canDo = false;
+            leftArm01Prefab.transform.position = playerPosition.transform.position;
+            Debug.Log("SecondAccess");
+        
+        }
+        else if (seconds > 125f && canDo == false)
+        {
+            leftArm01Prefab.transform.position = leftArmPositionPoint.transform.position;
+            Debug.Log("ThirdAcces");
+        }
 
     }
 }
