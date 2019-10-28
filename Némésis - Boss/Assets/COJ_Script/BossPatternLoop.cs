db8ff3fe1132e63ace2f77342d2133a5fb7bc142
Job Corentin <job.corentin@outlook.fr>
2019-10-28 19:26:01 +0100

    First pattern almost DONE !

--- Némésis - Boss/Assets/COJ_Script/BossPatternLoop.cs
+++ Némésis - Boss/Assets/COJ_Script/BossPatternLoop.cs
@@ -10,18 +10,30 @@ public class BossPatternLoop : MonoBehaviour
     protected int patternRef;
     protected int patternSaved;
     protected bool canTakeDamage = true;
-    protected float seconds;
     protected float weaponDamage = 5;
     public Slider healthBar;
     List<int> patternList = new List<int>();
 
+    protected GameObject leftArm01;
+
+
+    // leftArmPattern condition and object
     public GameObject leftArm01Prefab;
-    public Transform leftArmPositionPoint;
-    public Transform playerPosition;
+    protected float leftArm01Speed = 5f;
+    protected bool canDo01 = true;
+    public Transform leftSpawnArmPoint;
+    public Transform prepareAttackPoint;
+    public Transform leftArmPoint;
+    public Transform ImpactPoint;
+    public Vector3 pattern01FirstDir;
 
     // Start is called before the first frame update
     void Start()
     {
+        leftArm01 = Instantiate(leftArm01Prefab,leftSpawnArmPoint.transform.position,Quaternion.identity);
+        leftArmPoint = leftArm01.transform.GetChild(0).transform;
+        ImpactPoint = leftArm01.transform.GetChild(1).transform;
+
         for (int i = 0; i < 3; i++)
         {
             patternList.Add(i);
@@ -33,16 +45,15 @@ public class BossPatternLoop : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         healthBar.value = bossHealth;
-        seconds++;
-
-        if( patternRef == 0)
+        if(patternRef == 0 && canDo01 == true)
         {
-            Debug.Log(seconds);
-            leftArm01Pattern();
+            StartCoroutine(LeftArmPattern01());
         }
+       
+
     }
 
     void BossPatternSelection()
@@ -67,8 +78,7 @@ public class BossPatternLoop : MonoBehaviour
         if (patternRef == 0)
         {
             Debug.Log("Pattern ref = 0");
-            seconds = 0;
-
+            
         }
         else
         {
@@ -103,33 +113,31 @@ public class BossPatternLoop : MonoBehaviour
         bossHealth -= weaponDamage;
         canTakeDamage = false;
         Debug.Log("bosshealth =" + bossHealth);
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(0.5f);
         canTakeDamage = true;
 
     }
 
-
-    void leftArm01Pattern()
+    IEnumerator LeftArmPattern01()
     {
-        bool canDo = true;
-        if(seconds <= 75f)
-        {
-            leftArm01Prefab.transform.position = playerPosition.transform.position + new Vector3(0, 2, 0);
-            Debug.Log("wait for it");
-            
-        }
-        else if (seconds <= 125f && canDo == true)
-        {
-            canDo = false;
-            leftArm01Prefab.transform.position = playerPosition.transform.position;
-            Debug.Log("SecondAccess");
-        
-        }
-        else if (seconds > 125f && canDo == false)
-        {
-            leftArm01Prefab.transform.position = leftArmPositionPoint.transform.position;
-            Debug.Log("ThirdAcces");
-        }
-
+        Vector3 hit = new Vector3(0, -2, 0);
+        float hitSpeed = 5f;
+        Debug.Log("Wait for it");
+        pattern01FirstDir = prepareAttackPoint.position - leftArmPoint.position;
+        leftArm01.transform.Translate(pattern01FirstDir.normalized * leftArm01Speed * Time.deltaTime, Space.World);
+        yield return new WaitForSeconds(1.2f);
+        canDo01 = false;
+        Debug.Log("And it hits");
+        ImpactPoint.transform.position = leftArmPoint.transform.position - new Vector3(0,-2,0);
+        pattern01FirstDir = ImpactPoint.position - prepareAttackPoint.position;
+        leftArm01.transform.Translate(pattern01FirstDir.normalized * Time.deltaTime, Space.World);
+        yield return new WaitForSeconds(0.2f);
+        Debug.Log("And it's stuck");
+        leftArm01.transform.position = leftArmPoint.transform.position;
+        yield return new WaitForSeconds(2f);
+        Debug.Log("And it goes back");
+        leftArm01.transform.position = leftSpawnArmPoint.position;
     }
+
+  
 }
