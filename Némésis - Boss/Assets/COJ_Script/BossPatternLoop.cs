a8be0583cd014c59131031d7b0a96fb824236a5b
Job Corentin <job.corentin@outlook.fr>
2019-10-30 17:50:54 +0100

    First pattern is FINISHED feedback has been added

--- Némésis - Boss/Assets/COJ_Script/BossPatternLoop.cs
+++ Némésis - Boss/Assets/COJ_Script/BossPatternLoop.cs
@@ -21,18 +21,24 @@ public class BossPatternLoop : MonoBehaviour
     public GameObject leftArm01Prefab;
     protected float leftArm01Speed = 5f;
     protected bool canDo01 = true;
+    protected bool canDo02 = false;
+    protected bool canDo03 = false;
+    protected bool canDo04 = false;
     public Transform leftSpawnArmPoint;
     public Transform prepareAttackPoint;
     public Transform leftArmPoint;
     public Transform ImpactPoint;
     public Vector3 pattern01FirstDir;
-
+    public Rigidbody2D leftArm01Rb;
+    public GameObject impactPointSpawnPrefab;
+    public Color dashNowColor = Color.red;
+    public Color normalColor = Color.white;
     // Start is called before the first frame update
     void Start()
     {
         leftArm01 = Instantiate(leftArm01Prefab,leftSpawnArmPoint.transform.position,Quaternion.identity);
         leftArmPoint = leftArm01.transform.GetChild(0).transform;
-        ImpactPoint = leftArm01.transform.GetChild(1).transform;
+        leftArm01Rb = leftArm01.GetComponent<Rigidbody2D>();
 
         for (int i = 0; i < 3; i++)
         {
@@ -45,14 +51,22 @@ public class BossPatternLoop : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         healthBar.value = bossHealth;
+
+
+        // Pattern LeftArm01
         if(patternRef == 0 && canDo01 == true)
         {
-            StartCoroutine(LeftArmPattern01());
+            StartCoroutine(LeftArmPattern01Part1());
+            canDo01 = false;
         }
-       
+     
+        // Pattern Left Arm01
+
+
+
 
     }
 
@@ -78,7 +92,7 @@ public class BossPatternLoop : MonoBehaviour
         if (patternRef == 0)
         {
             Debug.Log("Pattern ref = 0");
-            
+            canDo01 = true;
         }
         else
         {
@@ -118,26 +132,78 @@ public class BossPatternLoop : MonoBehaviour
 
     }
 
-    IEnumerator LeftArmPattern01()
+    IEnumerator LeftArmPattern01Part1()
     {
-        Vector3 hit = new Vector3(0, -2, 0);
-        float hitSpeed = 5f;
-        Debug.Log("Wait for it");
-        pattern01FirstDir = prepareAttackPoint.position - leftArmPoint.position;
-        leftArm01.transform.Translate(pattern01FirstDir.normalized * leftArm01Speed * Time.deltaTime, Space.World);
-        yield return new WaitForSeconds(1.2f);
-        canDo01 = false;
-        Debug.Log("And it hits");
-        ImpactPoint.transform.position = leftArmPoint.transform.position - new Vector3(0,-2,0);
-        pattern01FirstDir = ImpactPoint.position - prepareAttackPoint.position;
-        leftArm01.transform.Translate(pattern01FirstDir.normalized * Time.deltaTime, Space.World);
-        yield return new WaitForSeconds(0.2f);
-        Debug.Log("And it's stuck");
-        leftArm01.transform.position = leftArmPoint.transform.position;
-        yield return new WaitForSeconds(2f);
-        Debug.Log("And it goes back");
-        leftArm01.transform.position = leftSpawnArmPoint.position;
+        float moveSpeed = 5f;
+        float pattern1Timer = 2f;
+
+        while(pattern1Timer > 0)
+        {
+            Debug.Log("Wait for it");
+            pattern01FirstDir = prepareAttackPoint.position - leftArmPoint.position;
+            leftArm01Rb.velocity = pattern01FirstDir * moveSpeed;
+            pattern1Timer -= Time.deltaTime;
+            if (pattern1Timer <= 0.3f && pattern1Timer > 0.1f)
+            {
+                leftArm01.GetComponent<SpriteRenderer>().material.color = dashNowColor;
+            }
+            else
+            {
+                leftArm01.GetComponent<SpriteRenderer>().material.color = normalColor;
+            }
+            yield return null;
+        }
+
+        StartCoroutine(LeftArmPattern01Part2());
+      
     }
 
+    IEnumerator LeftArmPattern01Part2()
+    {
+        
+        {
+            
+            float impactSpeed = 15f;
+            Debug.Log("And it hits");
+            GameObject impactPointSpawn = Instantiate(impactPointSpawnPrefab, ImpactPoint.transform.position, Quaternion.identity);
+            pattern01FirstDir = impactPointSpawn.transform.position - leftArmPoint.position;
+            leftArm01Rb.velocity = pattern01FirstDir * impactSpeed;
+            yield return new WaitForSeconds(0.1f);
+            Destroy(impactPointSpawn);
+            StartCoroutine(LeftArmPattern01Part3());
+        }
+       
+      
+    }
+
+    IEnumerator LeftArmPattern01Part3()
+    {
+        Debug.Log("and it's stuck");
+        pattern01FirstDir = new Vector3(0, 0, 0);
+        leftArm01Rb.velocity = pattern01FirstDir * 1;
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(LeftArmPattern01Part4());
+    }
+
+    IEnumerator LeftArmPattern01Part4()
+    {
+        float pattern1Timer = 1f;
+        float returnSpeed = 10f;
+        while (pattern1Timer > 0)
+        {
+            Debug.Log("And it's comes back");
+            pattern01FirstDir = leftSpawnArmPoint.position - leftArmPoint.position;
+            leftArm01Rb.velocity = pattern01FirstDir * returnSpeed;
+            pattern1Timer -= Time.deltaTime;
+            yield return null;
+        }
+        
+        pattern01FirstDir = new Vector3(0, 0, 0);
+        leftArm01Rb.velocity = pattern01FirstDir * 0;
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Back to mama");
+        leftArm01Rb.position = leftSpawnArmPoint.transform.position;
+        
+    }
   
 }
